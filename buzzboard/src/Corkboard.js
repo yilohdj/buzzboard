6e3328e4c9852a8065d78f7a5364fa4c8cb175a3
ellielsb <subin04lee@gmail.com>
2024-09-28 17:44:35 -0400

    codefix

--- buzzboard/src/Corkboard.js
+++ buzzboard/src/Corkboard.js
@@ -27,7 +27,7 @@ function Corkboard({posters}) {
     };
 
     const handleClick = (index) => {
-        setClickedIndex((prevIndex) == (prevIndex === index ? null : index));
+        setClickedIndex((prevIndex) => (prevIndex === index ? null : index));
     };
 
     const handleCategoryChange = (e) => {
