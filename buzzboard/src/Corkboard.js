b810294a6d90646d400724837f2ed42b9f1ad5cf
ellielsb <subin04lee@gmail.com>
2024-09-28 15:21:44 -0400

    Hover Effect

--- buzzboard/src/Corkboard.js
+++ buzzboard/src/Corkboard.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import './Corkboard.css';
 
 function Corkboard({ posters }) {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
     <div className="corkboard">
       {posters.map((poster, index) => (
-        <div key={index} className="poster">
+        <div key={index} className="poster" onMouseEnter={() => handleMouseEnter(index)}
+             onMouseLeave={handleMouseLeave}>
           <img src={poster.file} alt={`Poster ${index + 1}`} className="poster-image" />
         </div>
       ))}
