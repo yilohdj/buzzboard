0fd1d996f503c9586cd13b1ad58ea27736ea59d5
ellielsb <subin04lee@gmail.com>
2024-09-28 20:43:49 -0400

    styling2

--- buzzboard/src/Corkboard.js
+++ buzzboard/src/Corkboard.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import './Corkboard.css';
+import 'CorkboardStyling.js';
 
 function Corkboard({posters}) {
     const [hoveredIndex, setHoveredIndex] = useState(null);
