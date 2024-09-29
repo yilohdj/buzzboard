bd94c34fe7417ee0294b68fb7decb2491d1d3c7a
ellielsb <subin04lee@gmail.com>
2024-09-28 20:51:22 -0400

    Update CorkboardStyling.js

--- buzzboard/src/CorkboardStyling.js
+++ buzzboard/src/CorkboardStyling.js
@@ -7,17 +7,17 @@ function Corkboard({ posters }) {
             <div className="row corkboard">
                 {posters.map((poster, index) => (
                     <div className="col-lg-3 col-md-4 col-sm-6 mb-4 d-flex justify-content-center" key={index}>
-                        <div className="card poster">
-                            <img
+                        <div className="card poster" style={{ width: '500px', height: '700px' }}>
                             <img
                                 className="card-img-top poster-image"
                                 src={poster.image}
                                 alt={poster.title}
+                                style={{ height: '250px', objectFit: 'cover' }} // Optional styling for the image
                             />
-                            <div className="details-overlay">
+                            <div className="details-overlay p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)', color: 'white' }}>
                                 <h5 className="card-title">{poster.title}</h5>
                                 <h6 className="category">{poster.category}</h6>
-                                <p className="description">
+                                <p className="description" style={{ whiteSpace: 'normal' }}>
                                     {poster.description}
                                 </p>
                             </div>
